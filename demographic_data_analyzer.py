6e8eaaf04d7e6311a89832e75b456ed64456251c
selehaddin <sele4416@gmail.com>
2020-11-28 18:47:29 +0000

    🤭 half way point

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -3,27 +3,30 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv("adult.data.csv") #✔️
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df['race'].value_counts() #✔️
 
     # What is the average age of men?
-    average_age_men = None
+    dftemp = df.loc[df["sex"] == "Male"]
+    average_age_men = dftemp["age"].mean() #✔️
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    percentage_bachelors = 100.*df.education.eq('Bachelors').sum()/df.shape[0] #✔️
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
-    # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    # with and without `Bachelors`, `Masters`, or `Doctorate` #✔️
+    higher_education = df[(df["education"] == "Bachelors") | (df["education"] == "Masters") | (df["education"] == "Doctorate")].shape[0]
+    lower_education = df[~((df["education"] == "Bachelors") | (df["education"] == "Masters") | (df["education"] == "Doctorate"))].shape[0]
 
-    # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    # percentage with salary >50K #✔️
+    tempholder = df[((df["education"] == "Bachelors") | (df["education"] == "Masters") | (df["education"] == "Doctorate")) & (df["salary"] == ">50K")]
+    tempholder2 = df[~((df["education"] == "Bachelors") | (df["education"] == "Masters") | (df["education"] == "Doctorate")) & (df["salary"] == ">50K")]
+    higher_education_rich = 100.*tempholder.shape[0]/higher_education
+    lower_education_rich = 100.*tempholder2.shape[0]/lower_education
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
     min_work_hours = None
